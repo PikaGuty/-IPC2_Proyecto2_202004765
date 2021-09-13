320007b7eada93432baeb13f2bbd7962db8240fe
PikaGuty <78063271+PikaGuty@users.noreply.github.com>
2021-09-12 21:55:01 -0600

    primero

---
+++ Proyecto2.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+#
+root.title("Bitxelart")
+root.iconbitmap("favicon.ico")
+root.resizable(False,False)
+root.geometry("1000x600")
+
+tabControl = ttk.Notebook(root)
+
+tab1 = Frame(tabControl)
+tab1.config(bg="#B0B0B0", width=1000, height=600)
+tabControl.add(tab1, text = 'Cargar Archivo')
+
+tab2 = Frame(tabControl)
+tab2.config(bg="#B0B0B0", width=1000, height=600)
+tabControl.add(tab2, text = 'Analizar Archivo')
+
+tab3 = Frame(tabControl)
+tab3.config(bg="#B0B0B0", width=1000, height=600)
+tabControl.add(tab3, text = 'Ver Reportes')
+
+tab4 = Frame(tabControl)
+tab4.config(bg="#B0B0B0", width=1000, height=600)
+tabControl.add(tab4, text = 'Seleccionar Imagen')
+
+tab5 = Frame(tabControl)
+tab5.config(bg="#B0B0B0", width=1000, height=600)
+tabControl.add(tab5, text = 'Ver Imagen')
+
+tabControl.pack()
+
+root.mainloop()
